a3e93a43dbe18f2d086c8ce54f52c901b3cf77cc
careb <carolina.pereira@utec.edu.uy>
2025-06-22 14:58:01 -0300

    Commit de la estructura del proyecto

---
+++ src/main/java/com/seclandia/bancocentral/bancocentral/BancoCentralSecLandApplication.java
@@ -0,0 +1,13 @@
+package com.seclandia.bancocentral.bancocentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoCentralSecLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoCentralSecLandApplication.class, args);
+	}
+
+}
